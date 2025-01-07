118efa1f8fcac4392fe03112e0ae2d8c560e4965
Hussain Tinwala <coder.hussaintin@gmail.com>
2025-01-07 19:46:36 +0530

    Added script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+const addbtn= document.querySelector("#addbtn")
+const main= document.querySelector(".box")
+
+addbtn.addEventListener("click", function(){
+    addNote()
+})
+
+const addNote=()=>{
+    const note= document.createElement("div");
+    note.classList.add("note")
+    note.innerHTML=`
+    <div class="header">
+        <ion-icon name="save-outline"></ion-icon>
+        <!-- <ion-icon name="save"></ion-icon> -->
+        <ion-icon name="trash-outline"></ion-icon>
+    </div>
+    <textarea></textarea>
+    `;
+    
+    main.appendChild(note);
+}
